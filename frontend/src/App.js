8ac5cac15657db417a2480d4d677ad7500578881
vidura-12 <viduranirmal195@gmail.com>
2024-07-10 14:55:09 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,7 +30,7 @@ function App() {
        
         <Route path="/scheduler/home" element={<AdminLayout><SchedulerHome /></AdminLayout>} />
         <Route path="/travelagent/home" element={<AdminLayout><AgentHome /></AdminLayout>} />
-        
+        <Route path="/travelagent/home" element={<AdminLayout><AgentHome /></AdminLayout>} />
       </Routes>
     </Router>
   );
