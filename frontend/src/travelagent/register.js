9391fe837370643e4fa80c42b045aae639062ac9
Nimesha4 <141208678+Nimesha4@users.noreply.github.com>
2024-08-25 12:22:09 +0530

    new

--- frontend/src/travelagent/register.js
+++ frontend/src/travelagent/register.js
@@ -1,8 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './post.css';
 import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'flowbite-react';
 
 export default function Register() {
+
+    const [formData, setFormData] = useState({
+      name: '',
+      email: '',
+      address: '',
+      number: '',
+      experience: '',
+      language: '' ,
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const gudesubmit = async (e) => {
+      e.preventDefault();
+  
+      const guide = new FormData();
+      guide.append('name', formData.name);
+      guide.append('email', formData.email);
+      guide.append('address', formData.address);
+      guide.append('number', formData.number);
+      guide.append('experience', formData.experience);
+      guide.append('language', formData.language);
+  
+      try {
+        const response = await axios.post('http://localhost:8081/TourGuide/add', guide, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+
+        setFormData({
+          name: '',
+          email: '',
+          address: '',
+          number: '',
+          experience: '',
+          language: '' ,
+        });
+      } 
+      catch (error) {
+        console.error('Error submitting the form', error);
+      }
+    };
+
   return (
     <div> 
         <div 
@@ -19,37 +71,92 @@ export default function Register() {
       }}
     ><div className="form-container">
     <h2>Register Tour Guide </h2>
-    <form>
-      <div>
-        <label name="name">Travel Guide's Name :</label>
-        <input type="text" id="name" name="name" placeholder='Name of Travel Agent' />
-      </div>
-      <div>
-        <label name="email">Travel Guide's Email :</label>
-        <input type="email" id="email" name="email" placeholder='Email' />
-      </div>
-      <div>
-        <label name="address"> Office Address :</label>
-        <input type="text" id="address" name="address" placeholder='Office Address' />
-      </div>
-      <div>
-        <label name="contact"> Contact Number :</label>
-        <input type="tel" id="contact" name="contact" placeholder='Contact Number'/>
-      </div>
-      <div>
-        <label name="experience">Experience Years :</label>
-        <input type="text" id="experience" name="experience" placeholder='Experience Years' />
-      </div>
-         
-      <div>
-        <label name="Languages-Spoken">Languages Spoken :</label>
-        <input type="text" id="Languages-Spoken" name="Languages-Spoken" placeholder='Languages Spoken' />
-      </div>
-       
-        
-      <button type="submit">Register</button>
-    </form>
+
+    <form onSubmit={gudesubmit}>
+              <div className=" ">
+                <label htmlFor="name">Tour Guide Name</label>
+                <input
+                  type="text"
+                  className=""
+                  id="name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+                
+              </div>
+              <div className=" ">
+                <label htmlFor="email">Tour Guide Email</label>
+                <input
+                  type="email"
+                  className=""
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+                
+              </div>
+              <div className="">
+              <label htmlFor="address">Tour Guide Address</label>
+                <input
+                  type="text"
+                  className=""
+                  id="address"
+                  name="address"
+                  value={formData.address}
+                  onChange={handleChange}
+                  required
+                /> 
+              </div>
+              <div className="">
+              <label htmlFor="number">Tour Guide Number</label>
+                <input
+                  type="text"
+                  className=""
+                  id="number"
+                  name="number"
+                  value={formData.number}
+                  onChange={handleChange}
+                  required
+                /> 
+              </div>
+              <div className=" ">
+              <label htmlFor="experience">Tour Guide Experience</label>
+                <input
+                  type="text"
+                  className=""
+                  id="experience"
+                  name="experience"
+                  value={formData.experience}
+                  onChange={handleChange}
+                  required
+                /> 
+              </div>
+              <div className=" ">
+              <label htmlFor="language">Tour Guide Language</label>
+                <input
+                  type="text"
+                  className=""
+                  id="language"
+                  name="language"
+                  value={formData.language}
+                  onChange={handleChange}
+                  required
+                /> 
+              </div>
+
+              <a href="/travelagent/booktourist">
+          <Button color={"#1E201E" } className='button'>
+          Submit
+          </Button>
+        </a>
+  
+
+            </form>
   </div></div>  
     </div>
   );
-} 
+}
