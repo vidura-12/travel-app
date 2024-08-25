c8442d64a139afbea26373ae4d54a35fd58fd226
vidura-12 <viduranirmal195@gmail.com>
2024-08-25 15:21:05 +0530

    up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserLayout from './UserLayout';
 import AdminLayout from './AdminLayout';
+
 import TravelAgentLayout from './TravelAgentLayout';
+import LocationLayout from './LocationLayout';
+
 import Home from './com/home';
 import Location from './com/location';
 import Hotel from './com/hotel';
 import Feed from './com/feedback';
 import AdminHome from './admin/home';
 import AdminLogin from './admin/AdminLogin';
+import Tour from './com/tours';
 import SchedulerHome from './scheduler/home';
 import AgentHome from './travelagent/home';
 
@@ -18,13 +22,10 @@ import AgentDashboard from './travelagent/dashboard';
 import AgentCreatePost from './travelagent/createpost';
 import AgentBookTourist from './travelagent/booktourist';
 import AgentRegister from './travelagent/register';
+import AgentGHome from './com/guideHome';
  
 
 import Location_Home from './locationmanager/home';
-import AddEvent from './EventManager/addEvent';
-import EventList from './EventManager/eventCard';
-import EventLayout from './EventLayout';
-
 import Newlocation from './com/newLocation';
 import Schedulerlayout from './schedulerlayout';
 import Travelagency from './com/agency';
@@ -37,30 +38,34 @@ import SignUpPage from './com/SignUpPage'; // Import the SignUpPage component
 
 
 
-
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Login Route */}
+        <Route path="/LoginPage" element={<LoginPage />} />
+        
+        {/* Signup Route */}
+        <Route path="/signup" element={<SignUpPage />} />  {/* New signup route */}
+
         {/* User Routes */}
         <Route path="/home" element={<UserLayout><Home /></UserLayout>} />
         <Route path="/location" element={<UserLayout><Location /></UserLayout>} />
         <Route path="/hotel" element={<UserLayout><Hotel /></UserLayout>} />
         <Route path="/feed" element={<UserLayout><Feed /></UserLayout>} />
-
         <Route path="/tours" element={<UserLayout><Tour /></UserLayout>} />
-
         <Route path="/newLocation" element={<UserLayout><Newlocation /></UserLayout>} />
         <Route path="/agency" element={<UserLayout><Travelagency /></UserLayout>} />
+        <Route path="/guideHome" element={<UserLayout><AgentGHome /></UserLayout>} />
 
         <Route path="/packages" element={<UserLayout><Packages/></UserLayout>} />
-
 
 
         {/* Admin Routes */}
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin/home" element={<AdminLayout><AdminHome /></AdminLayout>} />
 
+        {/* Role-based Routes */}
 
        
         <Route path="/scheduler/home" element={<AdminLayout><SchedulerHome /></AdminLayout>} />
@@ -73,15 +78,9 @@ function App() {
         <Route path="/travelagent/booktourist" element={<TravelAgentLayout><AgentBookTourist/></TravelAgentLayout>} />
         <Route path="/travelagent/register" element={<TravelAgentLayout><AgentRegister/></TravelAgentLayout>} />
 
+
         <Route path="/scheduler/home" element={<Schedulerlayout><SchedulerHome /></Schedulerlayout>} />
         <Route path="/travelagent/home" element={<AdminLayout><AgentHome /></AdminLayout>} />
-        <Route path="/Location/home" element={<LocationLayout><Location_Home /></LocationLayout>} />
-
-
-        <Route path="/EventManager/addEvent" element={<EventLayout><AddEvent /></EventLayout>} />
-        <Route path="/EventManager/eventCard" element={<EventLayout><EventList /></EventLayout>} />
-
-
         <Route path="/LocationAdmin/home" element={<LocationLayout><Location_Home /></LocationLayout>} />
 
       </Routes>
@@ -89,4 +88,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;
--- frontend/src/com/guideHome.js
+++ frontend/src/com/guideHome.js
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
+
 import React from 'react';
-import './travelagent/st.css';
+
 
 export default function GuideHome() {
   return (
