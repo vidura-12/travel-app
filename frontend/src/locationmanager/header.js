0c5573b26498a44b655e7f55ca3bd7c9a5c7158b
vidura-12 <viduranirmal195@gmail.com>
2024-09-05 18:21:38 +0530

    table update

--- frontend/src/locationmanager/header.js
+++ frontend/src/locationmanager/header.js
@@ -1,36 +1,30 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Use useNavigate instead of useHistory
-import './Header.css'; // Import the custom CSS file
+import { useNavigate } from 'react-router-dom';
+import './Header.css'; 
 
 const Header = () => {
-    const navigate = useNavigate(); // Use useNavigate to programmatically navigate
+    const navigate = useNavigate();
 
     const handleLogout = () => {
-        // Remove token from localStorage
         localStorage.removeItem('token');
-        
-        // Optionally, you might want to clear other session-related data
-        // localStorage.removeItem('otherKey');
-
-        // Redirect to login page
         navigate('/admin/login');
     };
 
     return (
-        <ul className="navbar-custom">
-            <li className="menu-item">
-                <a className="nav-link" aria-current="page" href="/LocationAdmin/home">DashBoard</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="/LocationAdmin/LocationsSummary">Reacts & Comments</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="/LocationAdmin/profile">Profile</a>
-            </li>
-            <li className="nav-item ml-auto">
-                <a className="nav-link logout-link" href="#" onClick={(e) => { e.preventDefault(); handleLogout(); }}><button>Logout</button></a>
-            </li>
-        </ul>
+        <div className="navbar-custom">
+            <ul className="menu">
+                <li className="menu-item">
+                    <a className="nav-link" href="/LocationAdmin/home">DashBoard</a>
+                </li>
+                <li className="menu-item">
+                    <a className="nav-link" href="/LocationAdmin/LocationsSummary">Reacts & Comments</a>
+                </li>
+            </ul>
+            <div className="profile">
+                <button className="nav-link-profile" onClick={() => navigate('/LocationAdmin/profile')}>Profile</button>
+                <button className="nav-link-profile-logout" onClick={handleLogout}>LogOut</button>
+            </div>
+        </div>
     );
 }
 
