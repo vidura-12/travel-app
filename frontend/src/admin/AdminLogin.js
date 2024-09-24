2b80c874701d692f4f373cf52d2c8b9ca48faeae
vidura-12 <viduranirmal195@gmail.com>
2024-09-24 10:24:32 +0530

    Update AdminLogin.js

--- frontend/src/admin/AdminLogin.js
+++ frontend/src/admin/AdminLogin.js
@@ -47,7 +47,9 @@ const AdminLogin = () => {
         case 'location_manager':
           navigate('/LocationAdmin/home');
           break;
-        
+        case 'Schedule_Manager':
+         navigate('/LocationAdmin/home');
+          break;
         default:
           setError('Unknown role');
       }
