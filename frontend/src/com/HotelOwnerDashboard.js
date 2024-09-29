6cc027eefecb9ad298732b7bbcd9f4e5ae601749
Kalpa Vidusha Pathirana <kalpa.vidusha@gmail.com>
2024-09-29 13:58:23 +0530

    Hotel owner dash b

---
+++ frontend/src/com/HotelOwnerDashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HotelOwnerDashboard() {
+    return (
+        <div>
+            <h1>Hotel Owner Dashboard</h1>
+        </div>
+    );
+}
+
+export default HotelOwnerDashboard;
